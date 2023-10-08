dbc9dc6033b6d99e0dad24a97d8b985448540b43
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-07 20:17:17 -0400

    card for player

--- components/auction.tsx
+++ components/auction.tsx
@@ -4,7 +4,6 @@ import supabase from '@/config/supabase';
 
 export const Auction = () => {
   const [playerData, setPlayerData] = useState<any>([]);
-  const [selectedPosition, setSelectedPosition] = useState('');
   const [selectedRating, setSelectedRating] = useState('');
   const [showBidForm, setShowBidForm] = useState(false);
   const [bidAmount, setBidAmount] = useState('');
@@ -47,19 +46,14 @@ export const Auction = () => {
   }, []);
 
   useEffect(() => {
-    // Apply filters based on selectedPosition and selectedRating to playerData
+    
     let filteredData = playerData;
 
-    if (selectedPosition) {
-      filteredData = filteredData.filter((player:any) => player.position === selectedPosition);
-    }
 
     if (selectedRating) {
       filteredData = filteredData.filter((player:any) => player.rating === selectedRating);
     }
 
-    // Filter out players with selected === true
-  
 
     if (filteredData.length > 0) {
       const randomIndex = Math.floor(Math.random() * filteredData.length);
@@ -67,22 +61,22 @@ export const Auction = () => {
     } else {
       setSelectedPlayer(null);
     }
-  }, [selectedPosition, selectedRating, playerData]);
+  }, [selectedRating, playerData]);
 
   const handleTeamClick = async (teamId:any) => {
     setShowBidForm(true);
   
     if (selectedPlayer) {
-      // Prompt the user to input the bid amount
+      
       const inputAmount = prompt(`Enter the bid amount for ${selectedPlayer.id}`);
       
       if (inputAmount !== null) {
-        // Convert bidAmount to a number
+        
         const bidAmountNumber = parseFloat(inputAmount);
   
         if (!isNaN(bidAmountNumber)) {
           try {
-            // Fetch the current team amount from the database
+           
             const { data: team, error: teamError } = await supabase
               .from('Team')
               .select('teamAmount')
@@ -91,10 +85,10 @@ export const Auction = () => {
             
             if (teamError) {
               console.error(teamError);
-              return; // Handle the error
+              return; 
             }
   
-            // Update the teamAmount in the Team table
+        
             const updatedAmount = team.teamAmount - bidAmountNumber;
             const { error: updateError } = await supabase
               .from('Team')
@@ -103,10 +97,10 @@ export const Auction = () => {
             
             if (updateError) {
               console.error(updateError);
-              return; // Handle the error
+              return; 
             }
   
-            // Update the local state with the updated team amount
+           
             const updatedTeamData = [...teamData];
             const teamIndex = updatedTeamData.findIndex((team) => team.id === teamId);
             if (teamIndex !== -1) {
@@ -114,7 +108,7 @@ export const Auction = () => {
               setTeamData(updatedTeamData);
             }
   
-            // Update the selected status of the player in the formPlayer table
+            
             const { data: playerData, error: playerError } = await supabase
               .from('formPlayer')
               .update({ selected: true })
@@ -122,10 +116,9 @@ export const Auction = () => {
   
             if (playerError) {
               console.error(playerError);
-              return; // Handle the error
+              return; 
             }
   
-            // Insert a new record into the TeamPlayer table
             const { error: teamPlayerError } = await supabase
               .from('TeamPlayer')
               .upsert([
@@ -137,7 +130,7 @@ export const Auction = () => {
   
             if (teamPlayerError) {
               console.error(teamPlayerError);
-              return; // Handle the error
+              return; 
             }
             setChanges(Date.now()); 
             setShowBidForm(false);
@@ -149,69 +142,72 @@ export const Auction = () => {
     }
   };
   
-
-  const handlePositionChange = (event:any) => {
-    setSelectedPosition(event.target.value);
-  };
-
   const handleRatingChange = (event:any) => {
     setSelectedRating(event.target.value);
   };
 
   return (
-    <div className='w-[1440px] flex justify-between mx-auto'>
-      <div className=''>
-        <h2>Select a Team</h2>
-        <ul>
-          {teamData.map((team:any) => (
-            <li key={team.id}>
-              <button onClick={() => handleTeamClick(team.id)}>
-                {team.teamName}-{team.teamAmount}
-              </button>
-            </li>
-          ))}
-        </ul>
+    <div className='w-[1440px] flex mx-auto'>
+      
+        <div className='w-[80%] h-screen flex flex-col justify-center items-center'>
 
-        {showBidForm && (
-          <div className='bid-form'>
-            <h2>Place a Bid for Player {selectedPlayer && selectedPlayer.name}</h2>
-            <input
-              type='number'
-              placeholder='Bid Amount'
-              value={bidAmount}
-              onChange={(e) => setBidAmount(e.target.value)}
-            />
-            <button onClick={handleTeamClick}>Submit Bid</button>
-          </div>
-        )}
-      </div>
-      <div className=''>
-        <h2>Filter Players</h2>
-        <select value={selectedPosition} onChange={handlePositionChange}>
-          <option value="">All Positions</option>
-          <option value="Forward">Forward</option>
-          <option value="Midfielder">Midfielder</option>
-          <option value="Defender">Defender</option>
-          <option value="Goal Keeper">Goalkeeper</option>
-        </select>
 
-        <select value={selectedRating} onChange={handleRatingChange}>
-          <option value="">All Ratings</option>
-          <option value="Icon">Icon</option>
-          <option value="A">A</option>
-          <option value="B">B</option>
-          <option value="C">C</option>
-        </select>
+            {selectedPlayer && (
+                <div className='w-[40%]'>
+                    <div className='bg-gray-400 shadow-[15px_10px_40px_10px_rgba(0,0,0,0.5)] mx-auto px-10 my-auto '>
+                        <div className="py-4 my-2">
+                            <img
+                                className="object-center object-cover rounded-full h-40 w-40 mx-auto "
+                                src={selectedPlayer.image}
+                                alt="photo"
+                            />
+                        </div>
+                        <div className="text-center">
+                            <p className="text-xl text-gray-800 font-bold mb-2">{selectedPlayer.name}</p>
+                            <p className="text-lg text-gray-700 font-bold">{selectedPlayer.id}</p>
+                            <p className="text-md text-gray-600 font-semibold">{selectedPlayer.position}</p>
+                            <p className="text-md text-gray-600 font-semibold">{selectedPlayer.department}</p>
+                            <p className="text-md text-gray-600 font-semibold">Rating: {selectedPlayer.rating}</p>
+                        </div>
+                    </div>
+                </div>
+            )}
 
-        {selectedPlayer && (
-          <div className='mb-10'>
-            <h2>Randomly Selected Player:</h2>
-            <p>Name: {selectedPlayer.name}</p>
-            <p>Position: {selectedPlayer.position}</p>
-            <p>Rating: {selectedPlayer.rating}</p>
-          </div>
-        )}
-      </div>
+            <select value={selectedRating} onChange={handleRatingChange} className='mt-6 w-[40%]'>
+                <option value="">All Ratings</option>
+                <option value="Icon">Icon</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+            </select>
+        </div>
+
+
+        <div className='flex flex-col justify-center'>
+            <h2 className='font-bold'>Teams:</h2>
+            <ul>
+                {teamData.map((team:any) => (
+                <li key={team.id}>
+                    <button onClick={() => handleTeamClick(team.id)}>
+                    {team.teamName} - {team.teamAmount} coins
+                    </button>
+                </li>
+                ))}
+            </ul>
+
+            {showBidForm && (
+                <div className='bid-form'>
+                <h2>Place a Bid for Player {selectedPlayer && selectedPlayer.name}</h2>
+                <input
+                    type='number'
+                    placeholder='Bid Amount'
+                    value={bidAmount}
+                    onChange={(e) => setBidAmount(e.target.value)}
+                />
+                <button onClick={handleTeamClick}>Submit Bid</button>
+                </div>
+            )}
+        </div>
     </div>
   );
 };
