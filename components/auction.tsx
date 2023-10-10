b8542d5ad90dfdbcf8ed220b9f471ca66d996e26
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-10 06:33:05 -0400

    pass button css is done

--- components/auction.tsx
+++ components/auction.tsx
@@ -285,9 +285,9 @@ export const Auction = () => {
               ))}
             </tbody>
           </table>
-          <div className='flex items-center justify-center'>
+          <div className='w-full'>
             <button
-              className='px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600'
+              className='px-4 py-2 w-full mt-4 bg-red-500 text-white rounded hover:bg-red-600'
               onClick={handlePass}
             >
               Pass
