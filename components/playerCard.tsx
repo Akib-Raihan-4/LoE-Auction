7b1269ab46abed000f4976def722d313a2e671f8
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-10 04:33:00 -0400

    position and all added in the image

--- components/playerCard.tsx
+++ components/playerCard.tsx
@@ -25,20 +25,36 @@ export const PlayerCard = ({ name, url, position, rating, department }: any) => 
     <div className='relative'>
       <img src={url} alt="" className='w-[500px] h-[500px] absolute top-36 right-0 z-0' />
       <div className='relative shadow-[15px_10px_40px_10px_rgba(0,0,0,0.5)]'>
-        <img src="playerCardPNG.png" alt="" className='w-[800px] z-10' />
-        <h1
-          ref={nameRef}
-          className='absolute bottom-[230px] text-white font-extrabold text-2xl'
-          style={{ left: bottomPosition }}
-        >
-          {name}
-          {/* Shamsun Nahar Majumder Mahapar */}
-          {/* Mohammed Rafidul Islam */}
-          {/* Ayesha Binte Hasan Oishee bella  */}
-          {/* {rating} */}
-          {/* SHAFIN SHAHRIA */}
-          {/* MEHEDI SUREM */}
-        </h1>
+        <div>
+          <img src="playerCardPNG.png" alt="" className='w-[800px] z-10' />
+          <h1
+            ref={nameRef}
+            className='absolute bottom-[230px] text-white font-extrabold text-2xl'
+            style={{ left: bottomPosition }}
+          >
+            {name}
+            {/* Shamsun Nahar Majumder Mahapar */}
+            {/* Mohammed Rafidul Islam */}
+            {/* Ayesha Binte Hasan Oishee bella  */}
+            {/* {rating} */}
+            {/* SHAFIN SHAHRIA */}
+            {/* MEHEDI SUREM */}
+          </h1>
+        </div>
+        <div>
+          <h1 className='absolute top-[250px] left-[55px] text-white font-extrabold text-2xl'>
+            Position: {position === 'Goal Keeper' ? 'GK' : position}
+            {/* left-[110px] */}
+          </h1>
+          <h1 className='absolute top-[350px] left-[55px] text-white font-extrabold text-2xl'>
+            Category: {rating}
+            {/* left-[150px] */}
+          </h1>
+          <h1 className='absolute top-[450px] left-[55px] text-white font-extrabold text-2xl'>
+            Department: {department}
+            {/* left-[130px] */}
+          </h1>
+        </div>
       </div>
     </div>
   );
